6c6a014cd15c4d489c2c90907cff6c5c610d88c7
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-11-04 10:04:28 +0530

    all set

--- public/script.js
+++ public/script.js
@@ -3,6 +3,8 @@ let canvas = document.getElementById("canvas");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+var io = io.connect("http://localhost:8080/")
+
 let ctx = canvas.getContext("2d");
 
 window.onmousemove = (e) => {
@@ -12,11 +14,22 @@ window.onmousemove = (e) => {
 
    window.onmousedown = (e) => {
        ctx.moveTo(x,y);
+       io.emit('down', {x,y})
        mouseDown = true;
-   }
+   };
+
    window.onmouseup = (e) => {
        mouseDown = false;
-   }
+   };
+
+   io.on("ondraw" , ({x,y}) => {
+    ctx.lineTo(x,y);
+    ctx.stroke();
+   });
+
+   io.on("ondown" , ({x,y}) => {
+       ctx.moveTo(x,y);
+   })
 
     window.onmousemove = (e) =>{
         x = e.clientX;
@@ -24,6 +37,7 @@ window.onmousemove = (e) => {
     
         if(mouseDown)
         {
+            io.emit("draw", {x,y});
            ctx.lineTo(x,y);
            ctx.stroke();
         }
